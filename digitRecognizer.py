44242b32b1a649c3ecbfe07b8c63cec3b5e6c286
alcck <alcck2000@gmail.com>
2023-03-11 14:01:41 +0300

    An AI trained program that can recognize handwritten numbers.

--- digitRecognizer.py
+++ digitRecognizer.py
@@ -4,7 +4,7 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as k
+from keras import backend as K
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
